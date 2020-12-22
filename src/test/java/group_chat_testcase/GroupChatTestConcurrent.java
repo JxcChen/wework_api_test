7861bf1d0304851ae01c49f2f9724d7628432d44
chenjinxuan <360088940@qq.com>
2020-12-22 20:39:23 +0800

    增加群聊多线程用例

--- src/test/java/group_chat_testcase/GroupChatTestConcurrent.java
+++ src/test/java/group_chat_testcase/GroupChatTestConcurrent.java
@@ -2,23 +2,19 @@ package group_chat_testcase;
 
 import api_object.GroupChatObject;
 import api_object.TokenHelper;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.parallel.Execution;
 import utils.FakeUtils;
 
 import java.util.ArrayList;
 
-import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
-
-@Epic("企业微信借口测试用例")
-@Feature("群聊管理测试用例")
-public class GroupChatTest {
+public class GroupChatTestConcurrent {
 
     static ArrayList<String> userList = new ArrayList<>();
     static String chatId;
@@ -28,17 +24,9 @@ public class GroupChatTest {
         accessToken = TokenHelper.getToken("wwbcc92e0afe51b09e", "r7S5zFDDm_OvmrGmGJR4IMNpCMCMjfa4wfrsGD7lO-g");
     }
 
-    @BeforeEach
-    void createTestGroup(){
-        userList.add("ChenJinXuan");
-        userList.add("XiaoChen");
-        chatId = GroupChatObject.createGroupChat(accessToken, userList);
-    }
-
     @DisplayName("创建群聊")
-    @Description("创建群聊测试用例")
-    @Story("创建群聊")
-    @Test
+    @RepeatedTest(10)
+    @Execution(CONCURRENT)
     void createChatTest(){
         userList.add("ChenJinXuan");
         userList.add("XiaoChen");
@@ -48,29 +36,27 @@ public class GroupChatTest {
 
 
     @DisplayName("修改群聊")
-    @Description("修改群聊测试用例")
-    @Story("修改群聊")
-    @Test
+    @RepeatedTest(10)
+    @Execution(CONCURRENT)
     void updateGroupChatTest(){
-        Response response = GroupChatObject.updateGroupChat(chatId, "只是群聊", "ChenJinXuan", accessToken);
+        Response response = GroupChatObject.updateGroupChat("003", "只是群聊", "ChenJinXuan", accessToken);
         assertEquals("0",response.path("errcode").toString());
     }
+
 
     @DisplayName("获取群聊会话")
-    @Description("获取群聊会话测试用例")
-    @Story("获取群聊会话")
-    @Test
+    @RepeatedTest(10)
+    @Execution(CONCURRENT)
     void getChatSessionTest(){
-        Response response = GroupChatObject.getChatSession(accessToken, chatId );
+        Response response = GroupChatObject.getChatSession(accessToken, "003" );
         assertEquals("0",response.path("errcode").toString());
     }
 
-    @DisplayName("推送群聊信息")
-    @Description("推送群聊信息测试用例")
-    @Story("推送群聊信息")
-    @Test
+    @DisplayName("推送群聊消息")
+    @RepeatedTest(10)
+    @Execution(CONCURRENT)
     void sendMessage(){
-        Response response = GroupChatObject.sendGroupMessage(accessToken, chatId, "你好\\n你的快递到了请在前台查收","text");
+        Response response = GroupChatObject.sendGroupMessage(accessToken, "003", "你好\\n你的快递到了请在前台查收","text");
         assertEquals("0",response.path("errcode").toString());
     }
 }
